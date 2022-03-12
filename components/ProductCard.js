710254917bd25687256fa8fe3df8f4b59d431678
mariani <m.mariani@global-coding.com>
2022-03-12 15:09:40 +0800

    fix insta icon position

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -7,9 +7,9 @@ import TitleProduct from "./UI/TitleProduct";
 import Price from "./UI/Price";
 import PriceDiscount from "./UI/PriceDiscount";
 import DateProduct from "./UI/DateProduct";
+import Link from "next/link";
 
 const ProductCard = () => {
-
   return (
     <>
       <div className="block rounded-xl shadow-3xl bg-white w-full md:max-w-md border border-dark-100">
@@ -24,10 +24,21 @@ const ProductCard = () => {
             <PriceDiscount price={5000000} />
           </div>
           <div className="flex justify-between mt-3 items-center">
-            <DateProduct product="Jasa Perancangan Website e-Commerce"/>
-            <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
-              <Image src={InstaIcon} height={24} width={24} alt="Insta Icon"/>
-        
+            <DateProduct product="Jasa Perancangan Website e-Commerce" />
+
+            <a
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="contents"
+            >
+              <Image
+                className="cursor-pointer"
+                src={InstaIcon}
+                height={24}
+                width={24}
+                alt="Insta Icon"
+              />
             </a>
           </div>
         </div>
